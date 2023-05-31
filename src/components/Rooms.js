bfa0e6e46c83772de762c50eb2971c3a7454f1cf
Camsd <codecos.info@gmail.com>
2023-05-31 14:57:39 -0500

    rooms component

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, {useContext} from 'react';
+//context
+import { RoomContext } from '../context/RoomContext';
+//components
+import Room from '../components/Room';
 
 const Rooms = () => {
-  return <div>Rooms</div>;
+  const { rooms } = useContext(RoomContext);
+  return(
+  <section className='py-24'>
+    <div className="container mx-auto lg:px-0">
+      {/* grid */}
+      <div>
+        {rooms.map((room) => {
+          console.log(room);
+          return (<Room room={room} key={room.id} />);
+        })}
+      </div>
+    </div>
+  </section>
+  );
 };
 
 export default Rooms;

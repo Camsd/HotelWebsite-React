df0606529dcfd19f8fe508bed5209cd70773e41d
Camsd <codecos.info@gmail.com>
2023-06-01 15:15:50 -0500

    book form imports

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,7 +1,27 @@
 import React from 'react';
+//components
+import AdultsDropdown from '../components/AdultsDropdown';
+import KidsDropdown from '../components/KidsDropdown';
+import CheckIn from '../components/CheckIn';
+import CheckOut from '../components/CheckOut';
 
 const BookForm = () => {
-  return <div>BookForm</div>;
+  return <form className='h-[300px] bg-green-100 w-full lg:h-[70px]'>
+    <div className='flex flex-col w-full h-full lg:flex-row'>
+      <div>
+        <CheckIn />
+      </div>
+      <div>
+        <CheckOut />
+      </div>
+      <div>
+        <AdultsDropdown />
+      </div>
+      <div>
+        <KidsDropdown />
+      </div>
+    </div>
+  </form>;
 };
 
 export default BookForm;

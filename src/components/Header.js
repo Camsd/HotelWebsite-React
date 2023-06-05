fe4e9288b4c2d1d70ed8f2393a027fbbb36f1805
Camsd <codecos.info@gmail.com>
2023-06-05 11:09:37 -0500

    scroll header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+//logo
+import LogoWhite from "../assets/img/logo-white.svg";
+import LogoDark from "../assets/img/logo-dark.svg";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [header, setHeader] = useState(false);
+  useEffect(()=> {
+    window.addEventListener('scroll', ()=> {
+      window.scrollY > 50 ? setHeader(true) : setHeader(false);
+    })
+  })
+  return (
+    <div
+      className={`${
+        header ? "bg-white py-6 shadow-lg" : "bg-transparent py-8"
+      } fixed z-50 w-full transition-all duration-300`}
+    >
+      Header
+    </div>
+  );
 };
 
 export default Header;

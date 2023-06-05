da7a833a997b73160d0f2336b8c54bf26e018b00
Camsd <codecos.info@gmail.com>
2023-06-05 16:28:50 -0500

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React from "react";
+//logo
+import LogoWhite from "../assets/img/logo-white.svg";
 
 const Footer = () => {
   return (
-  <div>Footer</div>
+    <footer className="bg-primary py-12">
+      <div className="container mx-auto text-white flex justify-between">
+        {/* Logo */}
+        <a href="/">
+          <img src={LogoWhite} alt="" />
+        </a>
+        Copyright &copy. All rights reserved.
+      </div>
+    </footer>
   );
 };
 

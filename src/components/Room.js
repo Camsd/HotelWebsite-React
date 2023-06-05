1057af5695f5b9e2659b03f9d6f15e0ba10afa57
Camsd <codecos.info@gmail.com>
2023-06-05 11:01:27 -0500

    filter by num of  people

--- src/components/Room.js
+++ src/components/Room.js
@@ -8,7 +8,6 @@ import  {BsArrowsFullscreen, BsPeople} from 'react-icons/bs';
 const Room = ({ room }) => {
   //destructure room
   const {id, name, image, size, maxPerson, description, price} = room;
-  console.log(room);
   return (<div className='bg-white shadow-2xl min-h-[500px] group'>
     {/* img */}
     <div className='overflow-hidden'>
--- src/context/RoomContext.js
+++ src/context/RoomContext.js
@@ -1,21 +1,34 @@
-import React,{createContext, useEffect, useState} from 'react';
+import React, { createContext, useEffect, useState } from "react";
 //data
-import { roomData } from '../data';
+import { roomData } from "../data";
 //create context
 export const RoomContext = createContext();
 
 const RoomProvider = ({ children }) => {
   const [rooms, setRooms] = useState(roomData);
-  const [adults, setAdults] =useState('1 Adult');
-  const [kids, setKids] =useState('0 Kids');
+  const [adults, setAdults] = useState("1 Adult");
+  const [kids, setKids] = useState("0 Kids");
   const [total, setTotal] = useState(0);
 
+  useEffect(() => {
+    setTotal(Number(adults[0]) + Number(kids[0]));
+  });
 
-  console.log(total);
-  return(
-  <RoomContext.Provider value={{ rooms, adults, setAdults, kids, setKids }}>
-    {children}
-  </RoomContext.Provider>
+  const handleClick = (e) => {
+    e.preventDefault();
+    // filter rooms based on total (person)
+    const newRooms = roomData.filter(room => {
+      return total <= room.maxPerson;
+    });
+    setRooms(newRooms)
+  };
+
+  return (
+    <RoomContext.Provider
+      value={{ rooms, adults, setAdults, kids, setKids, handleClick }}
+    >
+      {children}
+    </RoomContext.Provider>
   );
 };
 

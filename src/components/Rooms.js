ea258468bb9e27b21c6bf24bec27b9b8568e6865
Camsd <codecos.info@gmail.com>
2023-06-07 06:48:04 -0500

    scrollToTop component

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -12,12 +12,20 @@ const Rooms = () => {
     <section className="py-24">
       {/* overlay & spinner */}
       {loading && (
-        <div className="h-screen fixed bottom-0 top-0 bg-black/90 w-full z-50
-        flex justify-center items-center">
+        <div
+          className="h-screen fixed bottom-0 top-0 bg-black/90 w-full z-50
+        flex justify-center items-center"
+        >
           <SpinnerDotted color="white" />
         </div>
       )}
       <div className="container mx-auto lg:px-0">
+        <div className="text-center">
+          <div className="font-tertiary uppercase text-[15px] tracking-[6px]">
+            Hotel & Sdina
+          </div>
+          <h2 className="font-primary text-[45px] mb-4">Room & Suites</h2>
+        </div>
         {/* grid */}
         <div className="grid grid-cols-1 max-w-sm mx-auto gap-[30px] lg:grid-cols-3 lg:max-w-none lg:mx-0">
           {rooms.map((room) => {
